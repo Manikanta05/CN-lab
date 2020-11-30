b8ea97aaeaa71e82248edc94e20b82e8623de9be
Manikanta <50227418+Manikanta05@users.noreply.github.com>
2020-11-30 14:23:34 +0530

    Update rsa.java

--- rsa.java
+++ rsa.java
@@ -5,7 +5,9 @@ public static void main(String[] args)
 {
 Main rsa=new Main(); 
 int p = 0,q = 0;
-Scanner in=new Scanner(System.in); System.out.println("Enter the message to be encrypted: "); String message=in.nextLine();
+Scanner in=new Scanner(System.in);
+System.out.println("Enter the message to be encrypted: ");
+String message=in.nextLine();
 char[] msgA=message.toCharArray(); boolean check=false;
 while(!check)
 {
@@ -22,11 +24,14 @@ if(rsa.gcd(i,z)==1)
 e=i; break;
 }
 }
-System.out.println("The value of e: "+e); System.out.println("Public key: ("+e+", "+n+")"); int d=0;
+System.out.println("The value of e: "+e); 
+ System.out.println("Public key: ("+e+", "+n+")"); int d=0;
 for(d=2;d<z;d++)
 if((e*d)%z==1)
 break; System.out.println("d: "+d); int ch;
-int[] a=new int[message.length()]; int[] c=new int[message.length()]; int t=0;
+int[] a=new int[message.length()]; 
+ int[] c=new int[message.length()]; 
+ int t=0;
 for(int i=0;i<message.length();i++)
  
 {
@@ -36,8 +41,11 @@ for(int j=0;j<e;j++)
 t=(t*ch)%n;
 c[i]=t;
 }
-System.out.println("Message ASCII Cipher"); for(int i=0;i<message.length();i++)
-System.out.println(" "+msgA[i]+"\t "+a[i]+"\t "+c[i]); char cha[]=new char[message.length()]; System.out.println("Decrypting...");
+System.out.println("Message ASCII Cipher"); 
+ for(int i=0;i<message.length();i++)
+System.out.println(" "+msgA[i]+"\t "+a[i]+"\t "+c[i]); 
+ char cha[]=new char[message.length()]; 
+ System.out.println("Decrypting...");
 for(int i=0;i<message.length();i++)
 {
 ch=c[i]; t=1;
